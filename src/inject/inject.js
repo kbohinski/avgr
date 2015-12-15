e77b164421c5cd7645c4ba5c337e6f7c96eab0f5
Kevin Bohinski <kbohinski@gmail.com>
2015-12-15 01:51:15 -0500

    Got weighted sums working

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -18,67 +18,102 @@ chrome.extension.sendMessage({}, function (response) {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
 
+            /* If they are on a grade page, then do work */
             if (document.URL.search("grades") != -1) {
                 var right = document.getElementById("right-side");
-                var grades = document.getElementsByClassName("score_details_table");
-                var postedGrades = document.getElementsByClassName("grade");
-                var maxs = document.getElementsByClassName("possible");
+                var categorys = document.getElementsByClassName("group_total");
+                var assignments = document.getElementsByClassName("assignment_graded");
 
+                /* Because JS dosent have maps.... */
+                var groupWeights = [];
+                var groupNames = [];
+                var groupTotals = [];
+                var groups = [];
+                var maxs = [];
                 var avgs = [];
-                var posteds = []
-                var outOfs = [];
 
-                for (i = 0; i < postedGrades.length; i++) {
-                    var html = postedGrades[i].innerText;
-                    var tmp = html.split(" ");
-                    var n = tmp[6];
-                    if (isNaN(n)) {
-                        posteds.push("false");
-                    } else {
-                        posteds.push("true");
-                    }
+                /* Get assignment groups and group weights */
+                for (i = 0; i < categorys.length; i++) {
+                    var html = categorys[i].innerHTML;
+                    var tmp = html.split(">");
+                    var name = tmp[1];
+                    name = name.split("<");
+                    name = name[0].trim();
+                    tmp = html.split("weight\">");
+                    tmp = tmp[1].split("<");
+                    var weight = parseFloat(tmp[0].trim());
+                    weight /= 100;
+                    groupNames.push(name);
+                    groupWeights.push(weight);
                 }
 
-                /* i = 1 to skip the first table header */
-                for (i = 1; i < (maxs.length - 1); i++) {
-                    var html = maxs[i].innerHTML;
-                    if (html.search("Final") == -1) {
-                        outOfs.push(parseFloat(html.trim()));
-                    }
-                }
+                /* Get assignment info */
+                /*
+                 * tables are:
+                 * -Submission
+                 * -Grade Info
+                 * -Comments (Can ignore)
+                 * -Rubric (Can ignore)
+                 */
+                for (i = 0; i < assignments.length; i += 4) {
+                    /* Get HTML */
+                    var gradeHtml = assignments[i].innerHTML;
+                    var infoHtml = assignments[i + 1].innerHTML;
 
-                for (i = 0; i < grades.length; i++) {
-                    var html = grades[i].innerHTML;
-                    var tmp = html.split("data-aria=\"");
-                    tmp = tmp[1].split("_");
-                    if (tmp[0] == "comments") {
-                        /* Canvas Comments */
-                        continue;
-                    } else {
-                        /* Canvas Grades */
-                        tmp = html.split("<td>");
+                    /* Elements needed from gradeHtml */
+                    var max = 0;
+                    var group = "";
+
+                    /* Elements needed from infoHtml */
+                    var avg = 0;
+
+                    tmp = gradeHtml.split("context\">");
+                    tmp = tmp[1].split("<");
+                    group = tmp[0].trim();
+
+                    tmp = gradeHtml.split("points_possible\">");
+                    tmp = tmp[1].split("<");
+                    max = parseFloat(tmp[0].trim());
+
+                    if (infoHtml.trim() != "") {
+                        tmp = infoHtml.split("Mean:");
                         tmp = tmp[1].split("<");
-                        tmp = tmp[0].split(":");
-                        avgs.push(parseFloat(tmp[1].trim()));
+                        avg = parseFloat(tmp[0].trim());
+                    }
+
+                    groups.push(group);
+                    maxs.push(max);
+                    avgs.push(avg);
+                }
+
+                /* Calculates percentages for each group */
+                for (i = 0; i < groupNames.length; i++) {
+                    var groupSum = 0;
+                    var groupMax = 0;
+                    for (j = 0; j < avgs.length; j++) {
+                        if (groups[j] === groupNames[i]) {
+                            groupSum += avgs[j];
+                            groupMax += maxs[j];
+                        }
+                    }
+                    var groupAvg = groupSum / groupMax;
+                    groupAvg *= 100;
+                    groupTotals.push(groupAvg);
+                }
+
+                /* Calculates weighting */
+                var finalAvg = 0;
+                for (i = 0; i < groupTotals.length; i++) {
+                    if (!isNaN(groupTotals[i])) {
+                        finalAvg += (groupTotals[i] * groupWeights[i]);
+                    } else {
+                        /* Assume people will do well on ungraded sections :) */
+                        finalAvg += (100 * groupWeights[i]);
                     }
                 }
 
-                var avgSum = 0;
-                for (i = 0; i < avgs.length; i++) {
-                    avgSum += avgs[i];
-                }
-
-                var outOfSum = 0;
-                for (i = 0; i < outOfs.length; i++) {
-                    if (posteds[i] == "true") {
-                        outOfSum += outOfs[i];
-                    }
-                }
-
-                var avg = avgSum / outOfSum;
-                avg *= 100;
-
-                right.innerHTML = "<p class=\"student_assignment final_grade\" style=\"font-size: 1.2em;\">Class Average: " + avg.toFixed(2) + "%</p>" + right.innerHTML;
+                right.innerHTML = "<p class=\"student_assignment final_grade\" style=\"font-size: 1.2em;\">Class Average: " + finalAvg.toFixed(2) + "%</p>" + right.innerHTML;
+                right.innerHTML += "<br>Note: Class Average assumes people will do perfectly on ungraded/future assignments.";
             }
 
         }
